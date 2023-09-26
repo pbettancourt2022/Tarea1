103cf451004d2275ed14fb983344bb4190f4176e
pbettancourt2022 <pbettancourt2022@udec.cl>
2023-09-26 11:50:08 -0300

    Empezamos

---
+++ src/main/java/org/example/Tarea1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Tarea1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+public class OrdenCompra{
+    private 
+    private int fecha;
+    private String estado;
+}
